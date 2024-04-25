e21c523fc507c7756f48208792d98f2cfbf51a39
Peter Gazdik <peter.gazdik@gmail.com>
2024-04-25 19:47:02 +0200

    Moving session-based model configurator from ModelOracle

--- tribefire-platform-base/src/tribefire/cortex/module/loading/SessionBasedConfigurationModelFactory.java
+++ tribefire-platform-base/src/tribefire/cortex/module/loading/SessionBasedConfigurationModelFactory.java
@@ -11,11 +11,14 @@
 // ============================================================================
 package tribefire.cortex.module.loading;
 
-import com.braintribe.model.processing.meta.configuration.ConfigurationModelBuilderManagedImpl;
+import com.braintribe.common.artifact.ArtifactReflection;
+import com.braintribe.model.generic.reflection.Model;
+import com.braintribe.model.meta.GmMetaModel;
 import com.braintribe.model.processing.meta.configured.ConfigurationModelBuilder;
 import com.braintribe.model.processing.session.api.managed.ManagedGmSession;
 
 import tribefire.module.api.ConfigurationModelFactory;
+
 
 /**
  * @author peter.gazdik
@@ -36,6 +39,72 @@ import tribefire.module.api.ConfigurationModelFactory;
 	@Override
 	public ConfigurationModelBuilder create(String name, String version) {
 		return new ConfigurationModelBuilderManagedImpl(session, name, version);
+	}
+
+}
+
+class ConfigurationModelBuilderManagedImpl implements ConfigurationModelBuilder {
+
+	private final ManagedGmSession session;
+	private final GmMetaModel model;
+
+	/**
+	 * @param session
+	 *            The {@link ManagedGmSession} for queries and the new {@link GmMetaModel}.
+	 * @param modelName
+	 *            The name of the new model, its globalId will be `model:modelName`.
+	 */
+	public ConfigurationModelBuilderManagedImpl(ManagedGmSession session, String modelName, String version) {
+		this.session = session;
+		this.model = session.create(GmMetaModel.T, Model.modelGlobalId(modelName));
+		this.model.setName(modelName);
+		this.model.setVersion(version);
+	}
+
+	/**
+	 * @param session
+	 *            The {@link ManagedGmSession} for queries.
+	 * @param model
+	 *            An existing {@link GmMetaModel}, no new model will be created.
+	 */
+	public ConfigurationModelBuilderManagedImpl(ManagedGmSession session, GmMetaModel model) {
+		this.session = session;
+		this.model = model;
+	}
+
+	@Override
+	public ConfigurationModelBuilder addDependency(ArtifactReflection standardArtifactReflection) {
+
+		if (!standardArtifactReflection.archetypes().contains("model"))
+			throw new IllegalArgumentException("Artifact " + standardArtifactReflection + " is not a model");
+
+		return addDependencyByName(standardArtifactReflection.name());
+	}
+
+	@Override
+	public ConfigurationModelBuilder addDependencyByName(String modelName) {
+		return addDependencyByGlobalId(Model.modelGlobalId(modelName));
+	}
+
+	@Override
+	public ConfigurationModelBuilder addDependency(Model model) {
+		return addDependencyByGlobalId(model.globalId());
+	}
+
+	@Override
+	public ConfigurationModelBuilder addDependencyByGlobalId(String globalId) {
+		return addDependency((GmMetaModel) session.getEntityByGlobalId(globalId));
+	}
+
+	@Override
+	public ConfigurationModelBuilder addDependency(GmMetaModel model) {
+		this.model.getDependencies().add(model);
+		return this;
+	}
+
+	@Override
+	public GmMetaModel get() {
+		return this.model;
 	}
 
 }
